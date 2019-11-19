ee967df5e35f755cc5e9ee87b6d28c0b8d37a80d
Sanechka Sanechka <aleksandra.salyuk@gmail.com>
2019-11-19 21:45:35 +0200

    1

---
+++ GroupValidationTests/UnitTest1.cs
@@ -0,0 +1,21 @@
+using NUnit.Framework;
+using GroupValidation;
+
+namespace GroupValidationTests
+{
+    [TestFixture]
+    public class Tests
+    { 
+        [TestCase(null,"1")]
+        [TestCase("1",null)]
+        [TestOf(typeof(Group))]
+        [Ignore ("because")]
+        public void VerifyThatOblectIsNullWhenPropertyIsNull(string value, string value2)
+        {
+            Group createdGroup = Group.CreateGroup(value, value2, out string error);
+            //Warn.If(createdGroup, Is.EqualTo(null));
+            Assert.IsNull(createdGroup);
+            
+        }
+    }
+}
